5b8a555b57d3a01fde5a199edf8ca66f5d86a11a
MrRomanMaccaruni <rommacc@yahoo.it>
2018-07-04 15:11:49 +0200

    fix typos

--- examples/ex01_leeyang/ex01_leeyang.cpp
+++ examples/ex01_leeyang/ex01_leeyang.cpp
@@ -9,12 +9,12 @@ int main(){
 	tba_single<TBA_LEEYANG> tba(1000);
 	tba.init_all(10.,1e-8);
 	tba.show_setup();
-	tba.show_setup("setup.txt");
+	tba.save_setup("setup.txt");
 	tba.save_kernels("kernel.csv");
 	tba.solve();
 	tba.evaluate_c();
 	tba.show_report();
-	tba.show_report("report.txt");
+	tba.save_report("report.txt");
 	tba.save_results("results.csv");
 
 	cfunc_single<TBA_LEEYANG> cfunc(1000, 1e-8, 1, 20);
